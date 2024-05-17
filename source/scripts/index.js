d59b5ed5d11c217d1e307af5dcfdf1dbc0d34c00
Ancogni <156594617+Ancogni@users.noreply.github.com>
2024-05-17 17:39:10 +0300

    Добавляет управление слайдером (#10)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -15,3 +15,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+/* Управление слайдером */
+document.addEventListener('DOMContentLoaded', () => {
+  const sliderBefore = document.querySelector('.slider__before');
+  const rangeButton = document.querySelector('.slider__range-button');
+  const slider = document.querySelector('.example__slider');
+  let isDragging = false;
+
+  function toPercentString(value) {
+    return value + '%';
+  }
+
+  rangeButton.addEventListener('mousedown', (mousedownEvent) => {
+    mousedownEvent.preventDefault();
+    isDragging = true;
+
+    const onMouseMove = (mousemoveEvent) => {
+      if (isDragging) {
+        const sliderRect = slider.getBoundingClientRect();
+        let newLeft = mousemoveEvent.clientX - sliderRect.left;
+        newLeft = Math.max(0, newLeft);
+        newLeft = Math.min(newLeft, sliderRect.width);
+        const posPercentage = (newLeft / sliderRect.width) * 100;
+        sliderBefore.style.width = toPercentString(posPercentage);
+        rangeButton.style.left = toPercentString(posPercentage);
+      }
+    };
+
+    const onMouseUp = () => {
+      isDragging = false;
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp, { once: true });
+  });
+});
