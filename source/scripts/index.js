21ec2462ccf154aa591c103bfffe717e4298ef32
Ancogni <156594617+Ancogni@users.noreply.github.com>
2024-05-17 15:46:36 +0300

    Добавляет разметку главной страницы (#8)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+
+/* Открытие-закрытие меню навигации*/
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.querySelector('.main-nav__toggle');
+  const navList = document.querySelector('.main-nav__list');
+
+  toggleButton.addEventListener('click', function() {
+    navList.classList.toggle('main-nav__list--open');
+  });
+
+  document.addEventListener('click', function(e) {
+    if (!navList.contains(e.target) && !toggleButton.contains(e.target) && navList.classList.contains('main-nav__list--open')) {
+      navList.classList.remove('main-nav__list--open');
+    }
+  });
+});
