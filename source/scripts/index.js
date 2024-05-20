ad5278c29d6dfcfabbd23ccc4550aa07e35ad6cf
Ancogni <156594617+Ancogni@users.noreply.github.com>
2024-05-20 23:31:45 +0300

    Доделывает сайт (#11)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -7,64 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   toggleButton.addEventListener('click', () => {
     navList.classList.toggle('main-nav__list--open');
+    toggleButton.classList.toggle('main-nav__toggle--close');
   });
 
   document.addEventListener('click', (e) => {
     if (!navList.contains(e.target) && !toggleButton.contains(e.target) && navList.classList.contains('main-nav__list--open')) {
       navList.classList.remove('main-nav__list--open');
+      toggleButton.classList.remove('main-nav__toggle--close');
     }
-  });
-});
-
-/* Управление слайдером */
-document.addEventListener('DOMContentLoaded', () => {
-  const sliderBefore = document.querySelector('.slider__before');
-  const rangeButton = document.querySelector('.slider__range-button');
-  const slider = document.querySelector('.example__slider');
-  let isDragging = false;
-
-  function toPercentString(value) {
-    return value + '%';
-  }
-
-  function moveSlider(position) {
-    const sliderRect = slider.getBoundingClientRect();
-    let newLeft = position - sliderRect.left;
-    newLeft = Math.max(0, newLeft);
-    newLeft = Math.min(newLeft, sliderRect.width);
-    const posPercentage = (newLeft / sliderRect.width) * 100;
-    sliderBefore.style.width = toPercentString(posPercentage);
-    rangeButton.style.left = toPercentString(posPercentage);
-  }
-
-  rangeButton.addEventListener('mousedown', (event) => {
-    event.preventDefault();
-    isDragging = true;
-  });
-
-  rangeButton.addEventListener('touchstart', (event) => {
-    event.preventDefault();
-    isDragging = true;
-  });
-
-  document.addEventListener('mousemove', (event) => {
-    if (isDragging) {
-      moveSlider(event.clientX);
-    }
-  });
-
-  document.addEventListener('touchmove', (event) => {
-    if (isDragging) {
-      event.preventDefault();
-      moveSlider(event.touches[0].clientX);
-    }
-  });
-
-  document.addEventListener('mouseup', () => {
-    isDragging = false;
-  });
-
-  document.addEventListener('touchend', () => {
-    isDragging = false;
   });
 });
